7579b795eaa4eeac19ad7d94dbcb1ac324784744
mtrigueros <moisestrigueros@hotmail.com>
2025-01-31 18:07:52 -0600

    remove background

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -10,7 +10,7 @@ export default function Products({ selectedCategory }) {
   const productsToShow = products.filter(productFilter);
 
   return (
-    <section className="py-16 bg-gradient-to-b from-gray-100 via-white to-gray-50">
+    <section className="py-16">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {productsToShow.map((product) => (
