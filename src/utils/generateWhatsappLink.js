586e13c2bf7d5850e172a86af1cbe395ae404652
mtrigueros <moisestrigueros@hotmail.com>
2025-02-09 18:11:29 -0600

    track whatsapp eevent

--- src/utils/generateWhatsappLink.js
+++ src/utils/generateWhatsappLink.js
@@ -2,6 +2,10 @@ export function generateWhatsAppLinkForProducts(
   products,
   phoneNumber = '50557503212'
 ) {
+  if (window.fbq) {
+    window.fbq('track', 'Contact', { method: 'WhatsApp' });
+  }
+
   const header = 'Me interesan estos productos:\n'
   const productMessages = products.map((product) => {
     return `\nProducto: ${product.name}\nPrecio: C$${product.price}\nDescripcion: ${product.description}`
