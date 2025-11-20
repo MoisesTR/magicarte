7f23e67b0c805c4d94e92f37979babba4836d778
Moises-TR <moisestrigueros@hotmail.com>
2025-11-20 00:17:56 -0600

    scroll to top when going to product details

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -36,6 +36,7 @@ export default function ProductCard({ product }) {
 
   const handleCardClick = () => {
     navigate(`/product/${product.id}`)
+    window.scrollTo(0, 0);
   }
 
   const handleImageClick = (e, image) => {
